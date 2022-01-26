758878ee55f833dd8b4640a5093ee84a72e08c59
EmilyDSarani <emily.sarani@gmail.com>
2022-01-26 15:28:22 -0600

    one more shot at getting the secrets to work

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('quotable routes', () => {
+  
   beforeEach(() => {
     return setup(pool);
   });
